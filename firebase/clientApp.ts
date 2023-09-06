bc2c38dd0e8cd58a4e27f7aed996e9ac86ffb432
Makara Udom Teu <makara@web-essentials.co>
2023-09-06 17:01:19 +0700

    Firebase config

--- firebase/clientApp.ts
+++ firebase/clientApp.ts
@@ -17,12 +17,12 @@ if(whichDB === "realtime") {
     };
 } else {
     clientCredentials = {
-        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+        apiKey: 'AIzaSyD73n-uXTYWGY1Pwb3VVuKFeyjzG6AjnuA',
+        authDomain: 'todo-325f4.firebaseapp.com',
+        projectId: 'todo-325f4',
+        storageBucket: 'todo-325f4.appspot.com',
+        messagingSenderId: '83698156822',
+        appId: '1:83698156822:web:827893ce709617e46ebc37',
     };
 }
 
